b17d034cb1ee4312ab9b695561eb3fd77a1c0b33
ehoze <39226647+ehoze@users.noreply.github.com>
2021-02-26 10:58:43 +0100

    Add files via upload

--- mainkcrsk.cpp
+++ mainkcrsk.cpp
@@ -1,60 +1,20 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
 #include <Windows.h>
 using namespace std;
-string ciag;
-int score;
 class mouseposition
 {
-                vector <string> positions;
-        public:
-                void injected();  
+        public: 
 				int filesave(const char *);           
 };
 
-void mouseposition::injected()
-{
-
-		fstream plik;  
-        string wprowadzany;
-        plik.open("B:\\Programowanie z Panem Fulaa\\Zadanie Mat. 63.1&2\\ciagi.txt", ios::in); 
-         if(plik.good()) 
-            while(!plik.eof()) 
-                  {
-                	plik >> wprowadzany;
-                	positions.push_back(wprowadzany);
-                   }
-                   plik.close();     
-        			
-}
-
-
 //Eryk Kucharski
-//int mouseposition::obliczanie()
-//{
-//	    while (1)
-//    {
-//        POINT cursorPos;
-//        GetCursorPos(&cursorPos);
-//        float x = 0;
-//        x = cursorPos.x;
-//        float y = 0;
-//        y = cursorPos.y;
-//        if ((GetKeyState(VK_LBUTTON) & 0x8000) != 0)
-//        {
-//            cout << x << ',' << y << endl;
-//            
-//            Sleep(0);
-//            
-//        }
-//        Sleep(0);
-//    }
-//}
+
 int mouseposition::filesave(const char *nazwa)
 {	
         ofstream plik;
         plik.open(nazwa);
+        cout << "F12 - Koniec programu\n";
         while (1)
     	{
 	        POINT cursorPos;
@@ -78,17 +38,20 @@ int mouseposition::filesave(const char *nazwa)
 	            Sleep(0);  
 	            earlx = x;
 	        	early = y;
+	        	if (GetKeyState(VK_F12))
+	        	{
+	        		plik.close();
+	        		return 0;
+				}
 	        }
 	        Sleep(0);
-    	}
-        plik.close();        
+    	}      
 }
 
 int main()
 {
 //Eryk Kucharski
 	mouseposition x;
-    x.injected();
     x.filesave("B:\\Programowanie z Panem Fulaa\\Pozycja myszy\\wynik.txt");
     return 0;
 }
